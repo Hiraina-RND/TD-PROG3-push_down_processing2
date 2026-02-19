b34f230d7995e9bea72c199184d8e244729a5365
Hiraina <hei.hiraina@gmail.com>
2026-02-19 17:19:40 +0300

    feat: add countAllVotes method on DataRetriever and visual test on Main

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -1,2 +1,28 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataRetriever {
+    long countAllVotes() {
+        DBConnection dbConnection = new DBConnection();
+        String sql = """
+                select COUNT(vote.id) as total_vote
+                from vote
+                """;
+        long result = 0;
+
+        try (
+                Connection connection = dbConnection.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet resultSet = ps.executeQuery()
+        ){
+            if (resultSet.next()) {
+                result = resultSet.getLong("total_vote");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing query", e);
+        }
+        return result;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    static void main() {
+        DataRetriever dataRetriever = new DataRetriever();
+        System.out.println("totalVote = " + dataRetriever.countAllVotes());
+    }
 }
